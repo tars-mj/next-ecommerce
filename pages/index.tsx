e2de82bdaa98966d075fa6a9bdec1d88c4b60b99
Maciej Judka <maciej.judka@gmail.com>
2022-02-24 14:48:52 +0100

    Update styles in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const Home = () => {
       <Header />
       <main className="max-w-5xl mx-auto w-full flex-grow p-6 grid gap-6 sm:grid-cols-2">
         <img src="https://picsum.photos/id/1060/536/354" alt="Barista nalewający kawę do Chemexa" />
-        <p>
+        <p className="text-slate-900">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor facilis cupiditate
           molestiae magni eius nesciunt, nemo eligendi nam, doloremque placeat voluptatibus
           quibusdam consequatur possimus optio officia consectetur quos sint? Voluptatum.
