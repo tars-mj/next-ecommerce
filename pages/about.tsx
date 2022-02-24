f435a57d95959281e48db6eb9b21aa3112e562d8
Maciej Judka <maciej.judka@gmail.com>
2022-02-24 14:49:37 +0100

    Update AboutPage

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,13 @@
+import { Footer } from '../components/Footer';
 import { Header } from '../components/Header';
 
 const AboutPage = () => {
   return (
-    <>
+    <div className="flex flex-col min-h-screen bg-teal-100">
       <Header />
-    </>
+      <main className="max-w-5xl mx-auto w-full flex-grow p-6 grid gap-6 sm:grid-cols-2"></main>
+      <Footer />
+    </div>
   );
 };
 
