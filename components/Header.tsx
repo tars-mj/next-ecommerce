e5117efcc2e5a6dbf80807cddb984ca1fc186afa
Maciej Judka <maciej.judka@gmail.com>
2022-02-24 14:57:45 +0100

    Update Header with new NavLink component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,22 @@
-import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React from 'react';
+import { NavLink } from './NavLink';
 
 export const Header = () => {
+  const { asPath } = useRouter();
+  console.log(asPath);
   return (
     <header className="max-w-5xl mx-auto w-full">
       <nav className="bg-gray-500 px-4 py-2">
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
+        <NavLink exact href="/">
+          <a className="m-2">Home</a>
+        </NavLink>
+        <NavLink href="/about">
+          <a className="m-2">About</a>
+        </NavLink>
+        <NavLink href="/products">
+          <a className="m-2">Products</a>
+        </NavLink>
       </nav>
     </header>
   );
