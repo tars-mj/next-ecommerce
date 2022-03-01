7bf92945facaf75cb41301896a16e7f3cf252efa
Maciej Judka <maciej.judka@gmail.com>
2022-03-01 14:23:39 +0100

    Refactoring with new components

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,11 +1,12 @@
 import { Footer } from 'components/Footer';
 import { Header } from 'components/Header';
+import { Main } from 'components/Main';
 
 const AboutPage = () => {
   return (
     <div className="flex flex-col min-h-screen bg-teal-100">
       <Header />
-      <main className="max-w-5xl mx-auto w-full flex-grow p-6 grid gap-6 sm:grid-cols-2"></main>
+      <Main>About page</Main>
       <Footer />
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,24 @@
 import { Footer } from 'components/Footer';
 import { Header } from 'components/Header';
-import Image from 'next/image';
+import { Main } from 'components/Main';
+import { Product } from 'components/Product';
+
+const DATA = {
+  description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor facilis cupiditate
+	molestiae magni eius nesciunt, nemo eligendi nam, doloremque placeat voluptatibus
+	quibusdam consequatur possimus optio officia consectetur quos sint? Voluptatum.`,
+  thumbnailUrl: 'https://picsum.photos/id/1060/536/354',
+  thumbnailAlt: 'Barista nalewający kawę do Chemexa',
+  rating: 5.5
+};
 
 const Home = () => {
   return (
-    <div className="flex flex-col min-h-screen bg-teal-100">
+    <div className="flex flex-col min-h-screen ">
       <Header />
-      <main className="max-w-5xl mx-auto w-full flex-grow p-6 grid gap-6 sm:grid-cols-2">
-        <Image
-          src="https://picsum.photos/id/1060/536/354"
-          alt="Barista nalewający kawę do Chemexa"
-          width="500"
-          height="354"
-        />
-        <p className="text-slate-900">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor facilis cupiditate
-          molestiae magni eius nesciunt, nemo eligendi nam, doloremque placeat voluptatibus
-          quibusdam consequatur possimus optio officia consectetur quos sint? Voluptatum.
-        </p>
-      </main>
+      <Main>
+        <Product data={DATA} />
+      </Main>
       <Footer />
     </div>
   );
