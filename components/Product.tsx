f7cb7d49d48e5714c4131d175df85399c195faa6
Maciej Judka <maciej.judka@gmail.com>
2022-03-03 13:44:59 +0100

    Update Product with new types

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,17 +1,34 @@
 import { Rating } from 'components/Rating';
 import Image from 'next/image';
 
-interface ProductProps {
-  data: {
-    title: string;
-    description: string;
-    thumbnailUrl: string;
-    thumbnailAlt: string;
-    rating: number;
-  };
+interface ProductDetails {
+  title: string;
+  description: string;
+  thumbnailUrl: string;
+  thumbnailAlt: string;
+  rating: number;
 }
 
-export const Product = ({ data }: ProductProps) => {
+type ProductListItem = Pick<ProductDetails, 'title' | 'thumbnailAlt' | 'thumbnailUrl'>;
+
+interface ProductListItemProps {
+  data: ProductListItem;
+}
+
+interface ProductDetailsProps {
+  data: ProductDetails;
+}
+
+export const ProductListItem = ({ data }: ProductListItemProps) => {
+  return (
+    <>
+      <Image src={data.thumbnailUrl} alt={data.thumbnailAlt} width="500" height="354" />
+      <h2 className="p-4 text-3xl font-bold">{data.title}</h2>
+    </>
+  );
+};
+
+export const ProductDetails = ({ data }: ProductDetailsProps) => {
   return (
     <>
       <Image src={data.thumbnailUrl} alt={data.thumbnailAlt} width="500" height="354" />
