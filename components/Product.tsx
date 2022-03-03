77c4cb74f5a26b51edbce43d9b2048d840f48cba
Maciej Judka <maciej.judka@gmail.com>
2022-03-03 11:20:45 +0100

    Update Product page

--- components/Product.tsx
+++ components/Product.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 
 interface ProductProps {
   data: {
+    title: string;
     description: string;
     thumbnailUrl: string;
     thumbnailAlt: string;
@@ -14,7 +15,8 @@ export const Product = ({ data }: ProductProps) => {
   return (
     <>
       <Image src={data.thumbnailUrl} alt={data.thumbnailAlt} width="500" height="354" />
-      <p className="text-slate-900">{data.description}</p>
+      <h2 className="p-4 text-3xl font-bold">{data.title}</h2>
+      <p className="text-slate-900 p-4">{data.description}</p>
       <Rating rating={data.rating} />
     </>
   );
