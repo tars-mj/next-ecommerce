561d40e3dec3d5a5ed6837a2dcfc8916ba5f6f6b
Maciej Judka <maciej.judka@gmail.com>
2022-03-03 11:21:47 +0100

    Add new product page with csr sample

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import 'styles/globals.css';
 import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const client = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div>
-      <Component {...pageProps} />
+      <QueryClientProvider client={client}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
     </div>
   );
 }
