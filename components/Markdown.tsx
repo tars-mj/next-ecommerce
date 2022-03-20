7e944c403af82343b59e7459db479687f0977801
Maciej Judka <maciej.judka@gmail.com>
2022-03-20 17:47:05 +0100

    feat: implement regx test for external link

--- components/Markdown.tsx
+++ components/Markdown.tsx
@@ -1,14 +1,21 @@
 import { MDXRemote } from 'next-mdx-remote';
 import Link from 'next/link';
 import { MarkdownResult } from 'utils/types';
+import { useRouter } from 'next/router';
 
 export const Markdown = ({ children }: { children: MarkdownResult }) => {
+  const router = useRouter();
+  console.log(router);
   return (
     <MDXRemote
       components={{
         a: ({ href, ...props }) => {
           if (!href) {
             return <a {...props}></a>;
+          }
+
+          if (/^http/.test(href)) {
+            return <a {...props} href={href} title="external link" rel="noopener noreferrer"></a>;
           }
 
           return (
