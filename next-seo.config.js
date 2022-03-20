d1014b81ac89b6c35773b63192335ffa841ebba4
Maciej Judka <maciej.judka@gmail.com>
2022-03-20 13:43:19 +0100

    feat: add config seo to main root

---
+++ next-seo.config.js
@@ -0,0 +1,14 @@
+const title = 'Nasz sklep od Zaiste Programuj';
+const decription = 'Nasz sklep od Zaiste Programuj';
+
+const nextSeoConfig = {
+  title,
+  decription,
+  openGraph: {
+    url: `https://next-ecommerce-nine-inky.vercel.app/`,
+    title,
+    decription
+  }
+};
+
+export default nextSeoConfig;
